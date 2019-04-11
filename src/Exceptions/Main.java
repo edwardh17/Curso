7321e6fde55fd06659f3b0f74f179fddb207bf1c
Edward Hurtado <edwardh17@gmail.com>
2019-04-11 15:58:30 -0300

    172 Exceptions.mp4 video terminado

--- src/Exceptions/Main.java
+++ src/Exceptions/Main.java
@@ -1,5 +1,6 @@
 package Exceptions;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -9,13 +10,41 @@ public class Main {
 //        System.out.println(divdeLBYL(x,y));
 //        System.out.println(divideEAFP(x,y));
 //        System.out.println(divide(x, y));
-        int x = getInt();
+//        int x = getIntLBYL();
+        int x = getIntEAFP();
         System.out.println("x is " + x);
     }
 
     private static int getInt(){
         Scanner s = new Scanner(System.in);
         return s.nextInt();
+    }
+
+    private static int getIntLBYL(){
+        Scanner s = new Scanner(System.in);
+        boolean isValid = true;
+        System.out.println("Please enter an integer");
+        String input = s.next();
+        for(int i = 0; i<input.length(); i++){
+            if(!Character.isDigit(input.charAt(i))){
+                isValid = false;
+                break;
+            }
+        }
+        if(isValid){
+            return Integer.parseInt(input);
+        }
+        return 0;
+    }
+
+    private static int getIntEAFP(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter an integer");
+        try{
+            return s.nextInt();
+        }catch(InputMismatchException e){
+            return 0;
+        }
     }
 
     private static int divdeLBYL(int x, int y){
