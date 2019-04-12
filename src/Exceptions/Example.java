c5640fe09550300116299726bb0ce914a87f2c75
edwardh17@gmail.com <edw18782415>
2019-04-11 21:25:10 -0300

    se agregó clase Example en paquete Exceptions, video completo 173 Stack Trace and Call Stack

---
+++ src/Exceptions/Example.java
@@ -0,0 +1,23 @@
+package Exceptions;
+
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String[] args) {
+        int result = divide();
+        System.out.println(result);
+    }
+
+    private static int divide(){
+        int x = getInt();
+        int y = getInt();
+        System.out.println("x is " + x + ", y is " + y);
+        return x / y;
+    }
+
+    public static int getInt(){
+        Scanner s= new Scanner(System.in);
+        System.out.println("Please enter an integer ");
+        return s.nextInt();
+    }
+}
